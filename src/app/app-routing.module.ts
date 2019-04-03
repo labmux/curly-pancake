8da6beee908d0db836727fdf52ed1f8e9d23e284
Eliran Assaraf <eliran.a.bg@gmail.com>
2019-04-03 12:00:54 -0400

    added routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
 import { CounterComponent } from './views/counter/counter.component';
+import {DadJokesComponent} from './views/dad-jokes/dad-jokes.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'counter', component: CounterComponent},
-  // { path: 'dadJoke', component: DadJokeCompon}
+  { path: 'dadJokes', component: DadJokesComponent },
   {path: '**', pathMatch: 'full', component: NotFoundComponent}
 ];
 
